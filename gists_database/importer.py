eb7665103fbae87e06fc34674c1438d0c49b9a83
Caleb Davenport <xcmdprompt@gmail.com>
2018-07-30 16:14:25 +0000

    all tests passing

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,17 @@
 import requests
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    try:
+        ## get gists
+        req = requests.get('https://api.github.com/users/{}/gists'.format(username))
+        data = req.json()
+    except:
+        raise requests.HTTPError
+    
+    for gist in data:
+        ## iterate over gists
+        db.execute("INSERT INTO gists VALUES (?,?,?,?,?,?,?,?,?,?,?,?)",
+        [ None, gist["id"], gist["html_url"],
+        gist["git_pull_url"], gist["git_push_url"], gist["commits_url"],
+        gist["forks_url"], gist["public"], gist["created_at"],
+        gist["updated_at"], gist["comments"], gist["comments_url"] ])
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,29 @@
 from .models import Gist
 
 def search_gists(db_connection, **kwargs):
-    pass
+    if 'github_id' in kwargs:
+        params = {
+        'github_id': kwargs.get('github_id')
+        }
+        cursor = db_connection.execute("SELECT * FROM gists WHERE github_id = :github_id", params)
+        
+    elif 'created_at' in kwargs:
+        params = {
+        'created_at': kwargs.get('created_at')
+        }
+        
+        cursor = db_connection.execute("""
+          SELECT *
+          FROM
+            gists
+          WHERE
+            datetime(created_at) = datetime(:created_at)
+        """, params)
+    else:
+        cursor = db_connection.execute("SELECT * FROM gists")
+        
+    #turning tuple into the Gist object    
+    gist_list = []
+    for gist in cursor.fetchall():
+        gist_list.append(Gist(gist))
+    return gist_list
