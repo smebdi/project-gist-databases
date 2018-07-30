67665049b00a89f84038e90eeb4ce20194625a0f
Caleb Davenport <xcmdprompt@gmail.com>
2018-07-30 16:17:30 +0000

    clean up

--- gists_database/search.py
+++ gists_database/search.py
@@ -2,23 +2,11 @@ from .models import Gist
 
 def search_gists(db_connection, **kwargs):
     if 'github_id' in kwargs:
-        params = {
-        'github_id': kwargs.get('github_id')
-        }
+        params = {'github_id': kwargs.get('github_id')}
         cursor = db_connection.execute("SELECT * FROM gists WHERE github_id = :github_id", params)
-        
     elif 'created_at' in kwargs:
-        params = {
-        'created_at': kwargs.get('created_at')
-        }
-        
-        cursor = db_connection.execute("""
-          SELECT *
-          FROM
-            gists
-          WHERE
-            datetime(created_at) = datetime(:created_at)
-        """, params)
+        params = {'created_at': kwargs.get('created_at')}
+        cursor = db_connection.execute("""SELECT * FROM gists WHERE datetime(created_at) = datetime(:created_at)""", params)
     else:
         cursor = db_connection.execute("SELECT * FROM gists")
         
